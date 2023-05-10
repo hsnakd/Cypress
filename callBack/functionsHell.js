9273f12615aa83a304b6e5614980ea8f8f4a7381
hsnakd <hsnakd10@gmail.com>
2023-05-10 19:35:33 +0300

    add: terminal

--- callBack/functionsHell.js
+++ callBack/functionsHell.js
@@ -1,17 +1,18 @@
 /***
- * in Java: make Database connection and get some data, then you use this data inside your code
- * --- Connect DB
- * --- gets data
- * --- is waiting until the data is received 
- * --- then executes rest of the code
- * in JS: 
- *  --- while trying to connect DB
- *  --- the rest of the functions does not wait -- async code
+  in Java: make Database connection and get some data, then you use this data inside your code
+  --- Connect DB
+  --- gets data
+  --- is waiting until the data is received 
+  --- then executes rest of the code
+ 
+in JS: 
+  --- while trying to connect DB
+  --- the rest of the functions does not wait -- async code
  */
 
 function waitingForSomeServerToAct() {
     let dataNeeded;
-    setTimeout(() => {
+    setTimeout(() => {      // same Thread.sleep in Java 
         console.log("BackEnd Information Have Just Been Received");
         dataNeeded = "Cydeo";
         console.log(dataNeeded);
@@ -20,8 +21,8 @@ function waitingForSomeServerToAct() {
 }
 
 function functionThatNeedsBackEndData(info) {
-    console.log("I need to use BackEnd Data "+info);
+    console.log("I need to use BackEnd Data " + info);
 }
 
-let data = waitingForSomeServerToAct(); // calling server connection, getting the data and storing it to a variable
-functionThatNeedsBackEndData(data); // this function does not wait for the result of previous action
+let data = waitingForSomeServerToAct();     // calling server connection, getting the data and storing it to a variable
+functionThatNeedsBackEndData(data);         // this function does not wait for the result of previous action
