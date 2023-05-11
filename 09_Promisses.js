20b8ba12c8a15e595bdc7ad45bad1a06692edf62
hsnakd <hsnakd10@gmail.com>
2023-05-11 19:41:48 +0300

    Day 2 finished

--- 09_Promisses.js
+++ 09_Promisses.js
@@ -10,19 +10,37 @@ let stateOfTheWebPage = function (state) {
     })
 }
 
+
+console.log("----------------------------------");
+
+
 console.log(stateOfTheWebPage(true).then(function(result) {
-    console.log(result);
-}));
-console.log(stateOfTheWebPage(true).then((result) => {
-    console.log(result);
+    console.log(result);                                        // Promise { <pending> }    &    Page is loaded
 }));
 
+
+console.log("----------------------------------");
+
+
+console.log(stateOfTheWebPage(true).then((result) => {
+    console.log(result);                                        // Promise { <pending> }    &    Page is loaded
+}));
+
+
+console.log("----------------------------------");
+
+
 console.log(stateOfTheWebPage(false).catch(function(errorMsg) {
-    console.log(errorMsg);
+    console.log(errorMsg);                                        // Promise { <pending> }    &    Page is NOT loaded yet
 }
 ));
+
+
+console.log("----------------------------------");
+
+
 console.log(stateOfTheWebPage(false).catch((errorMsg) => {
-    console.log(errorMsg);
+    console.log(errorMsg);                                        // Promise { <pending> }    &    Page is NOT loaded yet
 }
 ));
 
@@ -32,17 +50,23 @@ console.log(stateOfTheWebPage(false).catch((errorMsg) => {
 })
  */
 
+
+
+console.log("----------------------------------");
+console.log("----------------------------------");
+
+
 new Promise((resolve,reject) => {
     setTimeout(() => {
         resolve(1)
     }, 3000);
 }).then((result) => {
-    console.log(result);  // 1
+    console.log(result);    // 1
     return result*2;
 }).then((result2) => {
-    console.log(result2); // 2
+    console.log(result2);   // 2
     return result2*2;
 }).then((result3) => {
-    console.log(result3); // 4
-    return result3 * 2; // the value increased to 8 but we did not use it to log
+    console.log(result3);   // 4
+    return result3 * 2;     // the value increased to 8 but we did not use it to log
 })
